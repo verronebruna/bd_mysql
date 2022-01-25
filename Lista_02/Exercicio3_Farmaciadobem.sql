89f294d68db387c2bda643fce24defcf9af7fb87
verronebruna <verronebruna@gmail.com>
2022-01-25 11:12:59 -0300

    Corrigindo exercicios

--- Lista_02/Exercicio3_Farmaciadobem.sql
+++ Lista_02/Exercicio3_Farmaciadobem.sql
@@ -7,48 +7,44 @@ use db_farmacia_do_bem;
 /*Crie uma tabela de categorias utilizando a habilidade de abstração e determine 3 atributos 
 relevantes do tb_categoria para se trabalhar com o serviço desta farmacia. Popule esta tabela Categoria com até 5 dados.*/
 create table tb_categoria(
-id bigint auto_increment,
-descricao varchar(255) not null,
+setor varchar(255) not null,
+
 primary key (id)
 );
 
-INSERT INTO tb_categoria (massa, borda, sabor) VALUES ("Fina", "Catupiry","Salgada");
+
+
+INSERT INTO tb_categoria (setor) VALUES ("antibiótico", true);
 
 /*Crie uma tabela de tb_produto e utilizando a habilidade de abstração e determine 5 atributos relevantes dos tb_produto para se 
-trabalhar com o serviço deste farmacia(não esqueça de criar a foreign key de tb_categoria nesta tabela). Popule esta tabela pizza com até 8 dados.*/
-create table tb_pizza( 
-id bigint auto_increment,
-recheio varchar(255) not null,
-valor int not null,
-tamanho varchar(255) not null,
-quantidade int not null,
-categoria_id bigint,
-primary key(id),
-foreign key(categoria_id) references tb_categoria (id) 
+trabalhar com o serviço deste farmacia(não esqueça de criar a foreign key de tb_categoria nesta tabela). */
+create table tb_produtos(
+	id bigint not null auto_increment,
+    nome varchar(255) not null,
+    dataCompra date,
+    quantidade int,
+    preco decimal(7,2) not null,
+    categoria_id bigint,
+    primary key (id),
+    foreign key (categoria_id) references tb_categoria (id)
 );
 
-INSERT INTO tb_pizza (recheio, valor, tamanho, quantidade, categoria_id) VALUES ("Vegetariana", 45.50, "Grande", 1, 1);
-INSERT INTO tb_pizza (recheio, valor, tamanho, quantidade, categoria_id) VALUES ("Sensação", 35.50, "Broto", 1, 2);
-INSERT INTO tb_pizza (recheio, valor, tamanho, quantidade, categoria_id) VALUES ("Palmito", 40.50, "Média", 1, 1);
-INSERT INTO tb_pizza (recheio, valor, tamanho, quantidade, categoria_id) VALUES ("Chocolate", 30.00, "Grande", 1, 2);
-INSERT INTO tb_pizza (recheio, valor, tamanho, quantidade, categoria_id) VALUES ("Negresco", 60.00, "Média", 2, 2);
-INSERT INTO tb_pizza (recheio, valor, tamanho, quantidade, categoria_id) VALUES ("Portuguesa", 55.00, "Grande", 1, 1);
-INSERT INTO tb_pizza (recheio, valor, tamanho, quantidade, categoria_id) VALUES ("Escarola", 40.50, "Broto", 1, 1);
-INSERT INTO tb_pizza (recheio, valor, tamanho, quantidade, categoria_id) VALUES ("Romeu e Julieta", 50.50, "Grande", 1, 1);
+INSERT INTO tb_produtos (nome, dataCadastro, quantidade, preco, categoria_id) VALUES ("Amoxilina", current_date(), 100, 19.90, 1);
+INSERT INTO tb_categoria (quantidade, preço, setor) VALUES ("1", "5.00","Primeiros socorros");
 
-select * from tb_pizza;
+select * from tb_produtos;
 
-/*Faça um select que retorne os Produtos com o valor maior do que 45 reais.*/
-select* from tb_pizza where valor > 45.00 ;
+/*SELECT PRODUTOS COM VALOR MAIOR QUE R$ 50.00*/
+select * from tb_produtos where preco > 50;
 
-/*Faça um select trazendo  os Produtos com valor entre 29 e 60 reais.*/
-select* from tb_pizza where valor between 29.00 and 60.00;
+/*SELECT PRODUTOS COM VALOR ENTRE 3 E 60 REAIS*/
+select * from tb_produtos where preco between 3 and 60;
 
-/*Faça um select utilizando LIKE buscando os Produtos com a letra C.*/
-select* from tb_pizza where recheio like "%c%";
+/*SELECT UTILIZANDO LIKE BUSCANDO PRODUTOS COM A LETRA B*/
+select * from tb_produtos where nome like "%b%";
 
-/*Faça um um select com Inner join entre  tabela categoria e pizza.*/
-select tb_pizza.recheio, tb_pizza.valor, tb_pizza.tamanho, tb_categoria.sabor, tb_categoria.borda, tb_categoria.massa from tb_pizza inner join tb_categoria on tb_categoria.id = tb_pizza.categoria_id; 
+/*FAÇA UM SELECT COM INNER JOIN ENTRE CATEGORIA E PRODUTO*/
+select * from tb_produtos inner join tb_categoria on tb_categoria.id = tb_produtos.categoria_id;
 
-/*Faça um select onde traga todos os Produtos de uma categoria específica (exemplo todos os produtos que são pizza doce).*/
-select tb_pizza.recheio, tb_pizza.valor, tb_pizza.tamanho, tb_categoria.sabor, tb_categoria.borda, tb_categoria.massa from tb_pizza inner join tb_categoria on tb_categoria.id = tb_pizza.categoria_id where tb_categoria.id = 2;
+/*FAÇA UM SELECT ONDE TRAGA TODOS OS PRODUTOS DE UMA CATEGORIA ESPECIFICA*/
+select * from tb_produtos inner join tb_categoria on tb_categoria.id = tb_produtos.categoria_id where tb_categoria.id = 3;
